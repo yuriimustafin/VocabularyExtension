0bb2552385bbe9f0391ac876237ce943dc4e185e
Yurii Mustafin <yuriimustafin@gmail.com>
2023-09-05 18:13:50 -0400

    Interfaces added

---
+++ VocabularyExtension.Core/Abstract/Infrastructure/ILearningHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocabularyExtension.Core.Abstract.Infrastructure
+{
+    public interface ILearningHistoryRepository
+    {
+        void GetRepetitions(DateTime from, DateTime to);
+        (int, int) GetRepetitionsGroupedCount(IEnumerable<int> wordIds);
+        (int, DateTime) GetStartsOfLearning(IEnumerable<int> wordIds);
+    }
+}
